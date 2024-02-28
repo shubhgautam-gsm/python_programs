e5f3196e97786f5902b31fe477120c7163ab4c99
shubhgautam-gsm <143082586+shubhgautam-gsm@users.noreply.github.com>
2024-02-28 17:30:19 +0530

    28-02-24

---
+++ unit 6 functions/calc.py
@@ -0,0 +1,34 @@
+# 1
+
+# #function definition
+# def calc(a,b):
+#   print('add is ',a+b)
+#   print('sub is ',a-b)
+#   print('div is ',a/b)
+
+# # function calling
+# calc(12,24)
+
+
+# 2
+
+# #function definition
+# def calc(a=2,b=5):
+#   print('add is ',a+b)
+#   print('sub is ',a-b)
+#   print('div is ',a/b)
+
+
+# # function calling
+# calc()
+
+
+#function definition
+def calc(a=2,b=5):
+  print('add is ',a+b)
+  print('sub is ',a-b)
+  print('div is ',a/b)
+
+
+# function calling
+calc(12,24)
---
+++ unit 6 functions/p2.py
@@ -0,0 +1,17 @@
+#function definition
+def add(a,b):
+ print("hello wold")
+ print(a+b)
+
+def sub(a,b):
+ print("hello wold")
+ print(a-b)
+
+def div(a,b):
+ print("hello wold")
+ print(a/b)
+ 
+# function calling
+add(10,12)
+sub(10,15)
+div(20,10)
