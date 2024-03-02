5bf6a1e9f6fa58e2b554cae148fb2917f8ae1c00
shubhgautam-gsm <143082586+shubhgautam-gsm@users.noreply.github.com>
2024-03-02 12:34:11 +0530

    02-03-24

---
+++ unit 6 functions/p25.py
@@ -0,0 +1,16 @@
+# A python callable() function in Python is something that can be called. This built-in
+# function checks and returns true if the object passed appears to be callable,
+# otherwise false.
+x = 8
+print('IS CALLABLE x',callable(x))
+
+
+def x():
+ a=10
+ b=20
+ c=a+b
+ return c
+ 
+ 
+    
+print('IS CALLABLE x()',callable(x))
